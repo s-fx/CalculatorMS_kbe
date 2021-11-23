de87a8b10f613ba7a4f2e9d602703648116db75f
s-fx <fxscholler@protonmail.com>
2021-11-23 11:08:37 +0100

    init project

---
+++ src/main/java/com/futureSheep/CalculatorMS_kbe/CalculatorMsKbeApplication.java
@@ -0,0 +1,13 @@
+package com.futureSheep.CalculatorMS_kbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorMsKbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorMsKbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futureSheep/CalculatorMS_kbe/CalculatorMsKbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futureSheep.CalculatorMS_kbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorMsKbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
